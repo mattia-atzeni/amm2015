ec0fbb7a697fd3d1e9f6345a3e7edca8a7b7d8c0
amm <amm@amm-VirtualBox>
2015-07-06 16:19:20 +0200

    secondo commit

---
+++ php/model/User.php
@@ -0,0 +1,55 @@
+<?php
+
+class User {
+   private $firstName;
+   private $lastName;
+   private $email;
+   private $username;
+   private $password;
+   
+   public function getFirstName() {
+       return $this->firstName;
+   }
+
+   public function getLastName() {
+       return $this->lastName;
+   }
+
+   public function getEmail() {
+       return $this->email;
+   }
+
+   public function getUsername() {
+       return $this->username;
+   }
+
+   public function getPassword() {
+       return $this->password;
+   }
+
+   public function setFirstName($firstName) {
+       $this->firstName = $firstName;
+   }
+
+   public function setLastName($lastName) {
+       $this->lastName = $lastName;
+   }
+
+   public function setEmail($email) {
+       $this->email = $email;
+   }
+
+   public function setUsername($username) {
+       if (!filter_var($username, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/[a-zA-Z]{5,}/')))) {
+            return false;
+        }
+        $this->username = $username;
+        return true;
+   }
+
+   public function setPassword($password) {
+       $this->password = $password;
+   }
+
+
+}
---
+++ php/view/master.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Title</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet"  type="text/css"  href="css/style.css">
+    </head>
+
+    <body>
+        <nav>
+            <ul>
+                <li><a href=""><img src="images/mooc.png" alt="mooc"></a></li>
+                <li><a class="navbar-item" href="" target="_blank">Info</a></li>
+            </ul>
+        </nav>
+        <br/>
+        <form id="login-form">
+            <h2>Login</h2>
+            <label for="email">Email</label><br/>
+            <input id="email" type="email" placeholder="Your Email" class="textbox">
+            <br/>
+            <label for="password">Password</label><br/>
+            <input id="password" type="password" placeholder="Your Password" class="textbox">
+            <br/>
+            <div class="submit-container">
+                <button type="submit">Login</button>
+            </div>
+        </form>
+        <footer>
+            <p>
+                Progetto di Amministrazione di Sistema 2015<br/>
+                Autore: Mattia Atzeni - Matricola: 48958
+            </p>
+        </footer>
+    </body>
+</html>
+
